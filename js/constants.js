cbb3114713c1f1fbe89aa675dd6b984a199ebbd3
Vladyslav <vladver1211@gmail.com>
2024-09-06 16:24:31 +0300

    add: grid tiles

--- js/constants.js
+++ js/constants.js
@@ -1,1 +1,20 @@
-const tileSize = 20;
+const boardWidth = 20; // default width of the board
+const boardHeight = 30; // default height of the board
+const tileSize = 20; // default tile size
+
+let boardArray = Array.from({ length: boardHeight }, () => [Array.from({ length: boardWidth }, () => 0)]);  // array of board elements
+
+
+
+
+
+
+
+const blockSquare = [
+    [1, 1],
+    [1, 1]
+]
+
+const blocks = [
+   blockSquare, 
+]
--- js/main.js
+++ js/main.js
@@ -1,2 +1,19 @@
-let array = Array.from({ length: 8 }, () => [Array.from({ length: 8 }, () => 0)]);
-console.log(array);
+const boardContainer = document.querySelector('#boardContainer');
+boardContainer.style.width = tileSize  * boardWidth + "px";
+boardContainer.style.height = tileSize  * boardHeight + "px";
+function drawTiles(){
+    boardContainer.innerHTML = ''; // Clear board container
+    for (let i = 0; i < boardWidth; i++){ // Draw tiles from current position
+        for (let j = 0; j < boardHeight; j++){
+            const tile = document.createElement("div");
+            tile.classList.add("tile");
+            tile.style.top = j * tileSize + "px";
+            tile.style.left = i * tileSize + "px";
+            tile.style.width = tileSize + "px";
+            tile.style.height = tileSize + "px";
+            boardContainer.appendChild(tile);
+        }
+    }
+}
+
+drawTiles();
